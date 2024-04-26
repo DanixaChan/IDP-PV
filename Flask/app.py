dbb9a83ee721c587c6acd1101bf13887f214c7e1
DanixaChan <dani.chandia@duocuc.cl>
2024-04-26 11:39:33 -0400

    Navbar añadido

--- Flask/app.py
+++ Flask/app.py
@@ -10,5 +10,25 @@ app = Flask(__name__, template_folder=template_dir, static_folder='../static')
 def index():
     return render_template('index.html')
 
+@app.route('/reg_ventas')
+def registros():
+    return render_template('registros.html')
+
+@app.route('/g_boleta')
+def gboleta():
+    return render_template('gboleta.html')
+
+@app.route('/go_despacho')
+def godespacho():
+    return render_template('godespacho.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
