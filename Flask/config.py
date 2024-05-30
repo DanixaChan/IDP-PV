a14ec683450f0914f5ebcc50b022b9550628f23d
DanixaChan <107137107+DanixaChan@users.noreply.github.com>
2024-05-29 20:26:46 -0400

    Update config.py
    URI modificado

--- Flask/config.py
+++ Flask/config.py
@@ -4,7 +4,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:Integracion123@database-integracion.ceglsznnib0i.us-east-1.rds.amazonaws.com:3306/integracion'
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user1:user123@172.31.27.200:3306/idp-pv-1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # URL del endpoint de AWS EC2 para obtener los datos de despacho
